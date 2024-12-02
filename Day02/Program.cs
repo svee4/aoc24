4685551f90abacfe069e6d71999501d365294802
Sandra Vepsä <sandravepsa@gmail.com>
2024-12-02 13:10:11 +0200

    Day 2 working span removeindex impl

--- Day02/Program.cs
+++ Day02/Program.cs
@@ -1,4 +1,6 @@
-﻿const string TestInput = """
+﻿using System.Reflection.Metadata.Ecma335;
+
+const string TestInput = """
 7 6 4 2 1
 1 2 7 8 9
 9 7 6 2 1
@@ -51,32 +53,48 @@ foreach (var line in input.Split(Environment.NewLine))
 
     if (safe)
     {
-        Console.WriteLine($"{string.Join(", ", levels)}: SAFE");
+        //Console.WriteLine($"{string.Join(", ", levels)}: SAFE");
         part1++;
     }
     else
     {
-        Console.WriteLine($"{string.Join(", ", levels)}: UNSAFE");
+        //Console.WriteLine($"{string.Join(", ", levels)}: UNSAFE");
     }
 }
 
-Console.WriteLine($"Part 1: {part1}");
+Console.WriteLine($"Part 1: {part1 == 421}");
 
 
 var part2 = 0;
 
 foreach (var line in input.Split(Environment.NewLine))
 {
-    var levels = line.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+    var report = line.Split(' ', StringSplitOptions.RemoveEmptyEntries)
         .Select(int.Parse)
-        .ToList();
+        .ToArray();
 
-    var safe = IsSafeReport(levels);
-    for (int i = 0; i < levels.Count && !safe; i++)
+    var report2 = report.ToList();
+
+    var safe = IsSafeReportSpan(report);
+    var safe2 = IsSafeReport(report2);
+
+    if (safe != safe2)
     {
-        var temp = levels.ToList();
-        temp.RemoveAt(i);
-        safe = IsSafeReport(temp);
+        Console.WriteLine($"EXPECTED: {safe2}, ACTUAL: {safe} FOR INPUT {string.Join(", ", report)}");
+    }
+
+    for (var i = 0; !safe && i < report.Length; i++)
+    {
+        safe = IsSafeReportSpan(report, removeIndex: i);
+
+        var report22 = report.ToList();
+        report22.RemoveAt(i);
+        safe2 = IsSafeReport(report22);
+
+        if (safe != safe2)
+        {
+            Console.WriteLine($"EXPECTED: {safe2}, ACTUAL: {safe} FOR INPUT {string.Join(", ", report)} WITH REMOVEINDEX {i}");
+        }
     }
 
     if (safe)
@@ -85,7 +103,57 @@ foreach (var line in input.Split(Environment.NewLine))
     }
 }
 
-Console.WriteLine($"Part 2: {part2}");
+if (part2 == 476)
+{
+    Console.WriteLine("Part2 ok");
+}
+else
+{
+    Console.WriteLine($"Part2 expected: 476 actual: {part2}");
+}
+
+static bool IsSafeReportSpan(ReadOnlySpan<int> report, int removeIndex = -1)
+{
+    var prev = removeIndex is 0 ? report[1] : report[0];
+
+    var isIncreasing = removeIndex switch
+    {
+        0 => report[1] < report[2],
+        1 => report[0] < report[2],
+        _ => report[0] < report[1],
+    };
+
+    for (var i = removeIndex is 0 ? 2 : 1; i < report.Length; i++)
+    {
+        if (i == removeIndex)
+        {
+            continue;
+        }
+
+        var cur = report[i];
+        var diff = cur - prev;
+        prev = cur;
+
+        if (Math.Abs(diff) is < 1 or > 3)
+        {
+            return false;
+        }
+
+        if (isIncreasing)
+        {
+            if (diff < 0)
+            {
+                return false;
+            }
+        }
+        else if (diff > 0)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
 
 static bool IsSafeReport(List<int> report)
 {
